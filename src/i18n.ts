49a4dc0a7852e3b860427bdb761b4d7b41833c3d
Cerx <vojtech.duchacek@gmail.com>
2023-03-26 20:00:48 +0200

    less sketch

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,26 +1,16 @@
 import { createI18n } from 'vue-i18n'
 import {useAppStore} from "./stores/app";
-import type {App} from "vue";
 
-
-export async function installI18n(app: App) {
-
-    const messages = async () => {
-        return{
-            en: await import('./locales/en.json'),
-            cz: await import('./locales/cz.json'),
-        }
-    }
-
-    const i18n = createI18n({
-        legacy: false,
-        locale: useAppStore().locale,
-        fallbackLocale: 'en',
-        messages: await messages()
-    })
-    app.use(i18n);
-    return i18n;
+const messages = {
+    en: () => import('./locales/en.json'),
+    cz: () => import('./locales/cz.json'),
 }
 
+const i18n = createI18n({
+    legacy: false,
+    locale: () => useAppStore().locale,
+    fallbackLocale: 'en',
+    messages
+})
 
-export default installI18n
+export default i18n
--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,18 @@ import 'normalize.css'
 import { TroisJSVuePlugin } from 'troisjs';
 import router from "./routes";
 import {createPinia} from "pinia";
-import installI18n from "./i18n";
+import i18n from "./i18n";
 import {useAppStore} from "./stores/app";
 
-const app = createApp(App)
-app.use(createPinia())
-app.use(TroisJSVuePlugin);
-app.use(router)
-const i18n = installI18n(app);
-app.mount('#app');
+    const app = createApp(App)
+    app.use(createPinia())
+    app.use(TroisJSVuePlugin);
+    app.use(router)
+    app.use(i18n)
+    app.mount('#app');
 
-const store = useAppStore();
+
+// const store = useAppStore();
 // store.$subscribe((_locale, state) => {
 //     console.info(state.locale);
 //     i18n.global.locale = state.locale;
