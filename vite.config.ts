37773d0922f6ab181e56b212d516d1434eee01cb
Cerx <vojtech.duchacek@gmail.com>
2023-03-26 19:18:16 +0200

    fix?

--- vite.config.ts
+++ vite.config.ts
@@ -13,4 +13,12 @@ export default defineConfig({
     }
   },
   plugins: [vue()],
+  resolve: {
+    alias: [
+      {
+        find: 'vue-i18n',
+        replacement: 'vue-i18n/dist/vue-i18n.cjs.js',
+      }
+    ]
+  }
 })
